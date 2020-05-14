7b8e660a70d0fe18e9333d752a552d2fe0e19c82
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2020-05-14 15:22:14 -0400

    update and cleanup

---
+++ MapRandomizer/MapRandomizer/source/ModInit.cs
@@ -0,0 +1,17 @@
+﻿using Harmony;
+using System.Reflection;
+
+namespace MapRandomizer
+{
+
+    public static class Mod
+    {
+        public const string HarmonyPackage = "us.tbone.MapRandomizer";
+        public static void Init(string directory, string settingsJSON)
+        {
+            var harmony = HarmonyInstance.Create(HarmonyPackage);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
