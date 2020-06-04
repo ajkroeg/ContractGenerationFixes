8714fc483fafa2b2a5f82598e6ca6be7e8134b92
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2020-06-04 19:50:16 -0400

    patch to keep forced contracts from softlocking when coinciding with travel contract

---
+++ MapRandomizer/MapRandomizer/source/Patches/ContractPatch.cs
@@ -0,0 +1,34 @@
+﻿using Harmony;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using BattleTech.Data;
+using BattleTech;
+using JetBrains.Annotations;
+using System.Runtime.Remoting.Messaging;
+using System;
+using UnityEngine;
+
+namespace MapRandomizer.Patches
+{
+	static class MapRandomizerPatches_Contracts
+	{
+		[HarmonyPatch(typeof(BattleTech.SimGameState), "OnDayPassed")]
+		public static class OnDayPassed_Patch
+		{
+			public static void Postfix(SimGameState __instance, int timeLapse)
+			{
+				__instance.CompanyStats.AddStatistic<int>("HasTravelContract", 0);
+				if (__instance.HasTravelContract==true)
+                {
+					__instance.CompanyStats.Set<int>("HasTravelContract", 1);
+				}
+                else
+                {
+					__instance.CompanyStats.Set<int>("HasTravelContract", 0);
+				}
+
+			}
+		}
+	}
+}
